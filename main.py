f403d438ac6a4da034434136076d147bade40ffe
misomaniacsoup <misomaniacsoup@gmail.com>
2026-02-07 21:17:59 -0600

    2: Player object have been created

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import pygame
 from logger import log_state
 from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
+from player import PLAYER
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = PLAYER(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
-        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        log_state()
 
 if __name__ == "__main__":
     main()
