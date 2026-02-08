9ff522fc42dce90b2fa4ba2f6069b08f4bd3b490
misomaniacsoup <misomaniacsoup@gmail.com>
2026-02-08 00:21:11 -0600

    4: Asteroids have been added and can be collided with

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+import sys
 import pygame
 from logger import log_state
+from logger import log_event
 from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,8 +29,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for object in updatable:
+            object.update(dt)
+        for object in asteroids:
+            if player.collides_with(object):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit(.)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
         log_state()
